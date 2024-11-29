f4fd4b0e8a549eded3f5fe6c030350ea0a46c8d8
Mateusz Skarupski <skaiipii@gmail.com>
2024-11-29 15:03:36 +0100

    Add utils docstring

--- src/hyperelliptic/utils.py
+++ src/hyperelliptic/utils.py
@@ -1,12 +1,13 @@
 from .primes import PRIMES
 from random import randrange
 from math import gcd, log, floor
-from collections import Counter
 from itertools import combinations
 from functools import reduce
 
 
 def gf_operation(function):
+    """Decorator checking if a method taking elements of finite fields are defined over the same field"""
+
     def function_wrapper(a, b):
         is_field_operation = type(a) == type(b)
         if is_field_operation and a.gf != b.gf:
@@ -17,7 +18,7 @@ def gf_operation(function):
 
 
 def is_prime(p, k=32):
-    # Miller-Rabin primality test
+    """Miller-Rabin primality test"""
     if p < 2:
         return False
     if p == 2:
@@ -46,6 +47,7 @@ def is_prime(p, k=32):
 
 
 def all_factors(N):
+    """Returns set of all factors of N."""
     result = set([1, N])
     divisors = factors(N)
 
@@ -57,6 +59,7 @@ def all_factors(N):
 
 
 def factors(n):
+    """Returns factors of an integer provided as an argument"""
     factors = []
 
     while n > 1:
@@ -68,7 +71,7 @@ def factors(n):
 
 
 def factor(n, smoothness_bound=1):
-    # Factor small numbers with pollard's p - 1
+    """Factor small numbers with pollard's p - 1"""
     if is_prime(n):
         return n
 
